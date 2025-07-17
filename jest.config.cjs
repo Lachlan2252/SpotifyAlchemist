f52f7d4243dd548a20aec6b21cc8165bd1803087
Lachlan2252 <140129010+Lachlan2252@users.noreply.github.com>
2025-07-17 17:43:04 -0400

    Fix typos and testing setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
   testMatch: ['**/server/tests/**/*.test.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+      diagnostics: false
+    }
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/client/src/$1',
     '^@shared/(.*)$': '<rootDir>/shared/$1'
