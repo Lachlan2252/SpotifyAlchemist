ee07d47dd0cea5b0f01ef9cf34af6a37cb00c3b7
Lachlan2252 <140129010+Lachlan2252@users.noreply.github.com>
2025-07-17 16:58:40 -0400

    feat: enhance electron app

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 const path = require('path');
 const { spawn } = require('child_process');
 
@@ -12,11 +12,30 @@ function createWindow() {
     },
   });
 
+  // Open the developer tools automatically in development
+  if (process.env.NODE_ENV === 'development') {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+
+  // Open external links in the user's default browser
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
   if (process.env.NODE_ENV === 'development') {
     win.loadURL('http://localhost:5173');
   } else {
     win.loadFile(path.join(__dirname, '../dist/client/index.html'));
   }
+
+  // Prevent navigation outside the app
+  win.webContents.on('will-navigate', (event, url) => {
+    if (url !== win.webContents.getURL()) {
+      event.preventDefault();
+      shell.openExternal(url);
+    }
+  });
 }
 
 app.whenReady().then(() => {
